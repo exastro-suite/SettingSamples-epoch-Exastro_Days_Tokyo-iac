9227ff73bc5bb99586b6ec1969bfa705e80962ad
y-oka2 <y-okada2@nec.com>
2021-12-09 16:50:25 +0900

    update ddl

--- ddl/eventsite.sql
+++ ddl/eventsite.sql
@@ -22,7 +22,7 @@ CREATE TABLE events
 	event_overview varchar(4000),
 	event_date datetime NOT NULL,
 	event_venue varchar(4000),
-	is_deleted boolean NOT NULL,
+	delete_flag boolean NOT NULL,
 	PRIMARY KEY (event_id),
 	UNIQUE (event_id)
 );
@@ -33,7 +33,7 @@ CREATE TABLE mst_admins
 	admin_id int(10) unsigned NOT NULL AUTO_INCREMENT,
 	account_id varchar(256) NOT NULL,
 	account_pw varchar(256) NOT NULL,
-	is_deleted boolean NOT NULL,
+	delete_flag boolean NOT NULL,
 	PRIMARY KEY (admin_id),
 	UNIQUE (admin_id)
 );
@@ -43,7 +43,7 @@ CREATE TABLE mst_blocks
 (
 	block_id int(10) unsigned NOT NULL AUTO_INCREMENT,
 	block_name char(1) NOT NULL,
-	is_deleted boolean NOT NULL,
+	delete_flag boolean NOT NULL,
 	PRIMARY KEY (block_id),
 	UNIQUE (block_id),
 	UNIQUE (block_name)
@@ -56,7 +56,7 @@ CREATE TABLE mst_seminars
 	seminar_name varchar(256) NOT NULL,
 	seminar_overview varchar(4000),
 	capacity int unsigned,
-	is_deleted boolean NOT NULL,
+	delete_flag boolean NOT NULL,
 	PRIMARY KEY (mst_seminar_id),
 	UNIQUE (mst_seminar_id)
 );
@@ -67,7 +67,7 @@ CREATE TABLE mst_speakers
 	speaker_id int NOT NULL AUTO_INCREMENT,
 	speaker_name varchar(256) NOT NULL,
 	speaker_profile varchar(4000),
-	is_deleted boolean NOT NULL,
+	delete_flag boolean NOT NULL,
 	PRIMARY KEY (speaker_id),
 	UNIQUE (speaker_id)
 );
@@ -75,15 +75,15 @@ CREATE TABLE mst_speakers
 
 CREATE TABLE participants
 (
-	participants_id int(10) unsigned NOT NULL AUTO_INCREMENT,
+	participant_id int(10) unsigned NOT NULL AUTO_INCREMENT,
 	user_id varchar(256) NOT NULL,
 	user_name varchar(256) NOT NULL,
-	kinds_of_sso varchar(256) NOT NULL,
+	kind_of_sso varchar(256) NOT NULL,
 	registered_date datetime NOT NULL,
-	is_deleted boolean NOT NULL,
+	delete_flag boolean NOT NULL,
 	seminar_id int(10) unsigned NOT NULL,
-	PRIMARY KEY (participants_id),
-	UNIQUE (participants_id)
+	PRIMARY KEY (participant_id),
+	UNIQUE (participant_id)
 );
 
 
@@ -91,7 +91,7 @@ CREATE TABLE seminars
 (
 	seminar_id int(10) unsigned NOT NULL AUTO_INCREMENT,
 	start_datetime datetime NOT NULL,
-	is_deleted boolean NOT NULL,
+	delete_flag boolean NOT NULL,
 	mst_seminar_id int(10) unsigned NOT NULL,
 	block_id int(10) unsigned NOT NULL,
 	speaker_id int,
